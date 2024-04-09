01094a371708f34869a7ef2a36dc8d0274b6d338
HippoBoi <tagrifavc@gmail.com>
2024-04-09 00:24:15 -0400

    like and dislike to posts

--- src/components/LikeRatio.tsx
+++ src/components/LikeRatio.tsx
@@ -1,18 +1,35 @@
 import { Badge, HStack } from '@chakra-ui/react';
-import { FaRegThumbsUp, FaRegThumbsDown } from "react-icons/fa";
+import { clamp } from 'framer-motion';
+import { useState } from 'react';
+import { FaRegThumbsUp, FaRegThumbsDown, FaThumbsUp, FaThumbsDown } from "react-icons/fa";
+
+enum likeType {
+    LIKE,
+    DISLIKE,
+    NONE
+}
 
 interface Props {
     likes: number
 }
 
 const LikeRatio = ({ likes }: Props) => {
+    const [like, setLike] = useState<likeType>(likeType.NONE);
+
     const convertLikeRatio = () => {
-        return (likes/10) * 100;
+        const likeAdd = like === likeType.LIKE ? 1 : like === likeType.DISLIKE ? -1 : 0;
+        return clamp(0, 100, ((likes + likeAdd)/10) * 100);
     }
 
     return (
         <HStack>
-            <FaRegThumbsUp />
+            {like === likeType.LIKE 
+            ? <FaThumbsUp color='#4182fa' onClick={() => setLike(likeType.NONE)} /> 
+            : <FaRegThumbsUp onClick={() => setLike(likeType.LIKE)} />}
+
+            {like === likeType.DISLIKE 
+            ? <FaThumbsDown color='#e30c4b' onClick={() => setLike(likeType.NONE)} /> 
+            : <FaRegThumbsDown onClick={() => setLike(likeType.DISLIKE)} />}
             <Badge colorScheme={convertLikeRatio() > 69 ? 'green' : convertLikeRatio() > 39 ? 'yellow' : 'red'}>
                 {convertLikeRatio() + "%"}
             </Badge>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import logo from '../assets/Logo/logo.webp';
+import logo from '../assets/Logo/logo2.png';
 import { HStack, Image, Text } from '@chakra-ui/react';
 import SwitchTheme from './SwitchTheme';
 
 const NavBar = () => {
     return (
-        <HStack justifyContent={'space-between'}>
-            <Image src={logo} boxSize='60px'></Image>
+        <HStack justifyContent={"space-between"}>
+            <Image margin={"10px"} src={logo} boxSize='60px'></Image>
+            <Text color={'purple.200'} fontSize={"2xl"} as={"b"}>la red más social de todas</Text>
             <SwitchTheme></SwitchTheme>
         </HStack>
     )
